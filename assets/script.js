4ed1dbfe7a0c4f9f83729f1bab8fcad85653ea9c
Abuhatabweb <165955515+Abuhatabweb@users.noreply.github.com>
2025-04-17 20:07:55 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+let container = document.querySelector(".container")
+
+
+function active() {
+    container.classList.add("active")
+}
+function inactive() {
+    container.classList.remove("active")
+}
